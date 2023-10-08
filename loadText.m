cb30f399867b306072bb97841a10f3c72b8ca22e
William McMahon-Puce <william.mcm.p@gmail.com>
2023-10-08 23:06:26 +1100

    LoadText script

---
+++ loadText.m
@@ -0,0 +1,16 @@
+function extractedData = loadText(fileName, columnNumbers)
+    % Inputs:
+    %   - fileName: The name of the text file to be read.
+    %   - columnNumbers: A vector of column numbers to extract.
+
+    % Check if the file exists
+    if ~exist(fileName, 'file')
+        error('File not found: %s', fileName);
+    end
+    
+    % Read the data from the text file
+    data = importdata(fileName); % Use 'readtable' if your data is in a table format
+    
+    % Extract the specified columns
+    extractedData = data(:, columnNumbers);
+end
