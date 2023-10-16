5d9eebf5cd00f2a1999454b6a1317dbd377e6249
William McMahon-Puce <william.mcm.p@gmail.com>
2023-10-16 14:22:27 +1100

    Added MA(1) forecast method

---
+++ MA1Model.m
@@ -0,0 +1,134 @@
+function MA1Model (data, figureTitle)
+    % will plot the S(a) vs a plot used to get best guess of a
+
+    % MA(1) model forecasting
+    n = length(data);
+
+    % x_i = c + a * n_(i-1) + n_i
+    % Estimate the parameters - 2 parms
+    mu = mean(data); % average (contant C in model)
+    a = bestGuessA(data); % use conditional least squares to find a
+
+    % Calculating Residuals
+    e = zeros(n,1); % Residuals
+
+    for i = 2:n % e(1) = 0
+        e(i) = (data(i) - (mu + a * e(i - 1))); % observed - fitted
+    end
+
+    % Constructing the 95% prediction intervals for 20 steps ahead  
+    p = 20; % steps ahead
+    lower = zeros(p,1); % Lower bound error
+    upper = zeros(p,1); % upper bound error
+    data_forecast = zeros(p, 1); % predicited values
+ 
+    % Calculate the standard deviation of residuals
+    s = std(e); 
+
+    % The first values
+    data_forecast(1) = mu + a * e(n);
+    lower(1) = mu - 1.96 * s + a * e(n);
+    upper(1) = mu + 1.96 * s + a * e(n);
+    
+    % loop over each step ahead
+    for i=2:p
+        lower(i) = mu - 1.96 * s * sqrt(1 - a^2);
+        upper(i)= mu + 1.96 * s * sqrt(1 - a^2);
+        data_forecast(i) = mu;
+    end
+
+    % Plot the time series, fitted, forecast values, 95% prediction intervals for 20 steps ahead.
+    fig = figure;
+    set(fig, 'Name', " MA(1) Forecast of " + figureTitle, 'Position', [10, 10, 1100, 900]);
+
+    % Time series data
+    h1 = plot(data); 
+    hold on
+
+    % Forecasted values
+    h3 = plot(n + 1:n + p, data_forecast, 'b', 'LineWidth', 1);
+
+    % 95% prediction interval
+    h4 = plot(n + 1:n + p, upper,'r:', 'LineWidth', 2); % Upper error bound
+    plot(n + 1:n + p, lower,'r:', 'LineWidth', 2); % Lower error bound
+
+
+    legend([h1 h3 h4],figureTitle, 'Forecast - 20 steps ahead', '95% CI', 'Location', 'NorthWest');
+    title(['Fitted MA(1) model to ' + figureTitle])
+
+    % Validation of the model
+
+    m = floor(log(n)); % lags of the dataset
+
+    %  --- Validating Tests ---
+    % Ljung-Box test    - ACF of residuals is simular to the ACF of a white noise process up-to lag m
+    % Shapiro-Wilk test - Normality of the residuals
+    % Two Sided test    - Mean of the residuals is significantly different from zero.
+    %  ---                   ---
+
+    disp("Validation of the MA(1) model to " + figureTitle)
+
+    % Ljung-Box test
+    [h,p] = lbqtest(e, 'Lags', m, 'DOF', m-2 ); % 2 parameter was estimated (mu, a)
+
+    if p > 0.05
+        disp("Ljung-Box test p-value = " + p + " - residuals are not significantly different from a white noise process")
+    else
+        disp("Ljung-Box test p-value = " + p + " - residuals are significantly different from a white noise process")
+    end 
+
+    % Shapiro-Wilk test
+    [h,p] = swtest(e);
+
+    if p > 0.05
+        disp("Shapiro-Wilk test p-value = " + p + " - distribution of residuals are not significantly different from normal.")
+    else
+        disp("Shapiro-Wilk test p-value = " + p + " - distribution of residuals are significantly different from normal")
+    end 
+
+    % Two Sided test - using student T 
+    p = 2 * (1 - cdf('T', abs(mean(e) * sqrt(n) / std(e)), length(e)-1) ); % df = n - 1
+
+    if p > 0.05
+        disp("Two Sided test p-value = " + p + " - residual mean is not significantly different from zero")
+    else
+        disp("Two Sided test p-value = " + p + " - residual mean is significantly different from zero")
+    end 
+end
+
+
+function bestA = bestGuessA(data)
+    % Setting up the parameters
+    bins = 100; % number of guesses of a - higher values will give more accurate result
+    n = length(data);
+    mu = mean(data);
+
+    eSum = zeros(bins, 1); % Will store the sum for each guessed a value 
+    aRange = linspace(-1, 1, bins); % Range of a values
+
+    % Calculating the best sigma value
+    for j = 1:length(aRange)
+        a = aRange(j);
+        e = zeros(n, 1); % an empty array for the residuals
+        for i = 2:length(data)
+            e(i) = (data(i) - (mu + a * e(i - 1)))^2; % calcuate the residual ^ 2
+        end
+        eSum(j) = sum(e); % sum the residuals^2
+    end
+
+    [eMin, index] = min(eSum); % find the min of the S(a)
+    bestA = aRange(index); % the best gess for the a value
+
+    % Plot a vs eSum
+    fig = figure;
+    set(fig, 'Name', "Estimating a for the MA(1) process", 'Position', [10, 10, 800, 500]);
+
+    h1 = plot(aRange, eSum, 'DisplayName', "S(a)=\Sigma e^2"); % various vals of S(a) over a
+    hold on
+    h2 = plot(bestA, eMin, 'r*', 'DisplayName', "Best value of a = " + bestA); % the best a value
+    legend();
+    xlabel("a")
+    ylabel("S(a)=\Sigma e^2")
+    title(["Estimating a for MA(1) processe"]);
+
+end
--- main.m
+++ main.m
@@ -36,9 +36,9 @@ gold = loadExcel("E:\Users\William\Uni\Swinburne OneDrive\OneDrive - Swinburne U
 %  ------------------------
 
 % Plots each of the colums of data to establish what process to model with - random walk, or an AR(1), or a white noise process (?)
-initalPlots(process1, "Column 1 Time Series Data") % white noise
-initalPlots(process2, "Column 2 Time Series Data") % random walk
-initalPlots(process3, "Column 3 Time Series Data") % AR(1) process
+% initalPlots(process1, "Column 1 Time Series Data") % white noise
+% initalPlots(process2, "Column 2 Time Series Data") % random walk
+% initalPlots(process3, "Column 3 Time Series Data") % AR(1) process
 
 % --- What process to use??? ---
 % white noise   ->  mean  model
@@ -49,15 +49,16 @@ initalPlots(process3, "Column 3 Time Series Data") % AR(1) process
 % Each model function/script will output the validating tests allowing 
 % you to know if the model is appropiate for use with the provided time-series.
 
-meanModel(process1, "Column 1 Data")
+% meanModel(process1, "Column 1 Data")
 
-naiveModel(process2, "Column 2 Data")
+% naiveModel(process2, "Column 2 Data")
 
-AR1Model(process3, "Column 3 Data")
+% AR1Model(process3, "Column 3 Data")
 
 
 %  ------   Part 2   ------
 %    Fit the MA(1) model
 %  ------------------------
 
-initalPlots(MA1process, "Column 4 - Time Series Data - MA(1) process") % known to be a MA(1) process
+% initalPlots(MA1process, "Column 4 - Time Series Data - MA(1) process") % known to be a MA(1) process
+MA1Model(MA1process, "Column 4 Data");
